84b17442904aea6a89ae64f7ec0200ddeba270a1
Jared <jkjones.dev@gmail.com>
2016-03-06 22:17:27 -0600

    adding all portions to resume

--- JS/stats.js
+++ JS/stats.js
@@ -1,6 +1,15 @@
 
 var data = [
   {
+    value:100,
+    color:"#262626",
+    highlight: "#505050",
+    lable:""
+  },
+];
+
+var data2 = [
+  {
     value:75,
     color:"#262626",
     highlight: "#505050",
@@ -18,9 +27,13 @@ var options = {};
 
 var showGraph = function() {
   var ctx = document.getElementById("python").getContext("2d");
-  var pyChart = new Chart(ctx).Doughnut(data,options);
+  var pyChart = new Chart(ctx).Doughnut(data2,options);
   ctx = document.getElementById("java").getContext("2d");
   var javaChart = new Chart(ctx).Doughnut(data,options);
+  ctx = document.getElementById("c").getContext("2d");
+  var javaChart = new Chart(ctx).Doughnut(data2,options);
+  ctx = document.getElementById("html-css").getContext("2d");
+  var javaChart = new Chart(ctx).Doughnut(data2,options);
 }
 
 window.onload = function() {
