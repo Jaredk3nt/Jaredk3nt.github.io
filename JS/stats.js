bc9b8ce0779cfc7430810648779a62dd26ec4cc5
Jared <jkjones.dev@gmail.com>
2016-03-06 20:52:16 -0600

    js fixes

--- JS/stats.js
+++ JS/stats.js
@@ -1,11 +1,3 @@
-var Irelia = require('irelia');
-var irelia = new Irelia({
-  secure:true,
-  host: 'prod.api.pvp.net',
-  path: '/api/lol/',
-  key: '',
-  debug:true
-});
 
 var data = [
   {
@@ -26,22 +18,3 @@ var options = {};
 
 var ctx = document.getElementById("champChart").getContext("2d");
 var champChart = new Chart(ctx).Doughnut(data,options);
-
-irelia.getChampions('na', true, function(err, summoner){
-  if(err){
-    if(err.status.code == 429){
-                console.log(err.status.message);
-            } else if(err.status.code == 404){
-                console.log(err.status.message);
-            } else if(err.status.code == 500){
-                console.log(err.status.message);
-            } else {
-                console.log('Unknown error code');
-            }
-        } else {
-            console.log(err); // Non http error
-        }
-  }else{
-    document.getElementById("champs").innerText = ('Free champions: ' + champions.join(', ');
-  }
-});
