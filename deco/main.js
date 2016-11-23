4f7d33477bcf8f1d4e364db64a98fc9b31a7528e
Jared Jones <jkjones.dev@gmail.com>
2016-11-22 18:51:03 -0600

    adding deco site for testing

---
+++ deco/main.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+	var $wrap = $(".slides-wrapper"), slides = $(".slide").length, scrolling = false, currentSlide = 1;
+
+  /*****************************
+  ***** NAVIGATE FUNCTIONS *****
+  *****************************/
+	function manageClasses() {
+		$wrap.removeClass(function (index, css) {
+			return (css.match(/(^|\s)active-slide\S+/g) || []).join(' ');
+		});
+		$wrap.addClass("active-slide" + currentSlide);
+
+		scrolling = true;
+		setTimeout(function () {
+			scrolling = false;
+		}, 1500);
+	}
+	function navigateUp() {
+		if (currentSlide > 1) {
+			currentSlide -= 1;
+			manageClasses();
+		}
+	}
+
+	function navigateDown() {
+		if (currentSlide < slides) {
+			currentSlide += 1;
+			manageClasses();
+		}
+	}
+
+  /*********************
+  ***** MOUSEWHEEL *****
+  *********************/
+	$(document).on("mousewheel DOMMouseScroll", function (e) {
+		if (!scrolling) {
+			if (e.originalEvent.wheelDelta > 0 || e.originalEvent.detail < 0) {
+				navigateUp();
+			} else {
+				navigateDown();
+			}
+		}
+	});
+
+});
