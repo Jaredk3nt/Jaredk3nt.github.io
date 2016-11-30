faae88527ffe0ea79c3f34f6574fa846b06b6a10
Jared Jones <jkjones.dev@gmail.com>
2016-11-30 16:03:20 -0600

    adding updated DECO files

--- deco/main.js
+++ deco/main.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
 	var $wrap = $(".slides-wrapper"), slides = $(".slide").length, scrolling = false, currentSlide = 1;
 
-	// update active classes and control scroll timing
+  /*****************************
+  ***** NAVIGATE FUNCTIONS *****
+  *****************************/
 	function manageClasses() {
 		$wrap.removeClass(function (index, css) {
 			return (css.match(/(^|\s)active-slide\S+/g) || []).join(' ');
@@ -11,16 +13,15 @@ $(document).ready(function () {
 		scrolling = true;
 		setTimeout(function () {
 			scrolling = false;
-		}, 1300);
+		}, 1200);
 	}
-	// go up a slide
 	function navigateUp() {
 		if (currentSlide > 1) {
 			currentSlide -= 1;
 			manageClasses();
 		}
 	}
-	// go down a slide
+
 	function navigateDown() {
 		if (currentSlide < slides) {
 			currentSlide += 1;
@@ -28,21 +29,28 @@ $(document).ready(function () {
 		}
 	}
 
-  	var touchStartPos;
+  /*********************
+  ***** MOUSEWHEEL *****
+  *********************/
+	var touchStartPos;
 	$(document).on("mousewheel DOMMouseScroll", function (e) {
-		if (e.originalEvent.wheelDelta > 0 || e.originalEvent.detail < 0) {
-			navigateUp();
-		} else {
-			navigateDown();
+		if (!scrolling) {
+			if (e.originalEvent.wheelDelta > 0 || e.originalEvent.detail < 0) {
+				navigateUp();
+			} else {
+				navigateDown();
+			}
 		}
 	});
-
-	$(document).bind('touchstart', function (e){
+	
+	$(document).on('touchstart', function (e){
    		touchStartPos = e.originalEvent.touches[0].clientY;
+   		console.log("start");
 	});
 
-	$(document).bind('touchend', function (e){
+	$(document).on('touchend', function (e){
 	   var touchEndPos = e.originalEvent.changedTouches[0].clientY;
+	   console.log("end");
 	   if(!scrolling) {
 		   if(touchStartPos > touchEndPos+5){
 		      navigateDown();
